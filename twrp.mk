f8d0cb3d5b56ca3f6e33fa92ad48edc34bbfa1e6
Simon Shields <simon@lineageos.org>
2018-01-25 20:12:30 +1100

    midas: use evdev api for vibration in TWRP

--- twrp.mk
+++ twrp.mk
@@ -7,4 +7,7 @@ TARGET_RECOVERY_DEVICE_DIRS += $(LOCAL_PATH)/twrp
 TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
 TW_MAX_BRIGHTNESS := 32
 
+# use max77693-haptic device for vibration feedback
+TW_VIBRATOR_INPUT_DEV := /dev/input/event0
+
 TARGET_RECOVERY_INITRC := $(LOCAL_PATH)/twrp/recovery/root/init.recovery.midas.rc
